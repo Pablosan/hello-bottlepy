7465b99d161995e448a5219c8d3ebe3db7e2c5f5
Paul Nelson <Pablosan@SalientBlue.com>
2014-08-01 00:29:55 -0500

    tornado, configargparse integration

--- Dockerfile
+++ Dockerfile
@@ -12,9 +12,13 @@ FROM pablosan/bottle-py3
 
 # Install the app
 ADD hello-bottlepy /opt/hello-bottlepy
+ADD requirements.txt /opt/config/
+
+# Install python packages
+RUN pip3 install -r /opt/config/requirements.txt
 
 # Expose ports
-EXPOSE 8003
+EXPOSE 80
 
 # Define default command
-ENTRYPOINT ["/usr/bin/python3", "/opt/hello-bottlepy/server.py"]
+ENTRYPOINT /usr/bin/python3 /opt/hello-bottlepy/server.py
--- hello-bottlepy/server.py
+++ hello-bottlepy/server.py
@@ -1,8 +1,72 @@
-from bottle import route, run
+# Copyright (c) 2011-2014 Paul Nelson (Pablosan@SalientBlue.com)
+# All Rights Reserved.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-@route('/')
+"""HTTP (REST API) Server."""
+
+import logging
+import sys
+
+import bottle
+from configargparse import configargparse
+
+LOG = logging.getLogger(__name__)
+
+
+@bottle.route('/')
 def index():
-  return "<h1>Hello from bootlepy!</h1>"
+  return "<h1>Hello from bottlepy!</h1>"
 
-# You might want to make these values configurable
-run(host='0.0.0.0', port=8003, debug=True)
+
+def start(options):
+    """Start the server. Called by __main__."""
+    LOG.info("*** hello-bottlepy v%s ***", '0.0.1')
+
+    host = options.host or '0.0.0.0'
+    port = options.port or 80
+
+    # Start listening. Enable reload by default to pick up file changes
+    bottle.run(server='tornado', host=host, port=port, reloader=options.reloader)
+
+
+def main():
+    """Entrypoint for __main__."""
+
+    # Configuration options: config file, command line params, environment vars
+    parser = configargparse.ArgParser(default_config_files=['/etc/settings.ini'])
+    parser.add('--host',
+               help='host name (e.g. "mydomain.com", defaults to "0.0.0.0")',
+               env_var='HOST')
+    parser.add('-p', '--port', help='TCP port for the server. Defaults to 80',
+               env_var='PORT', type=int)
+    parser.add('-r', '--reload', dest='reloader', action='store_true',
+               help='Set auto-reload to True to automatically pick up file '
+               'changes. Defaults to False.',
+               env_var='RELOAD')
+    parser.set_defaults(reloader=False)
+
+    options = parser.parse_args()
+
+    # Output config info for easier setup and debugging
+    print(parser.format_help())
+    print("-----")
+    print(options)
+    print("-----")
+    parser.print_values()
+
+    # Start the server
+    start(options)
+
+
+if __name__ == "__main__":
+    main()
